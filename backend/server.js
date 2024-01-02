f0541f5821f87a693a960c29063974c51ba64d20
Divyanshu tandon <divyanshutandon@Divyanshus-MacBook-Air.local>
2024-01-02 11:20:27 +0530

    Initial Commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,42 @@
-const http = require('http');
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const contactRoute = require('./api/routes/contact');
+const dotenv = require('dotenv');
+const path = require('path');
 
-const app = require('./app');
+dotenv.config();
+
+const dbConnectionString = process.env.DB_CONNECTION_STRING;
+
+mongoose.connect(dbConnectionString);
+
+mongoose.connection.on('error', (error) => {
+  console.log('Connection failed:', error);
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Connected with database..');
+});
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/contact', contactRoute);
+
+const appDir = path.resolve();
+app.use(express.static(path.join(appDir, "/frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(appDir, "/frontend/build/index.html"))
+);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+const http = require('http');
 
 const server = http.createServer(app);
 
